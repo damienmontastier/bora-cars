0a05efb741ebb561d1e0e73c3ffbd20511f7dbe5
Damien Montastier <montastier.damien@gmail.com>
2026-04-06 21:45:34 +0200

    wip

---
+++ web/app/components/elements/Pitch.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app-elements-pitch">
+    <div class="app-elements-pitch__top">
+      <TextsP2 color="orange-100">
+        (Clé en main)
+      </TextsP2>
+      <TextsH2 color="orange-100">
+        Trouvez le véhicule qu'il vous faut, on s'occupe du reste.
+      </TextsH2>
+    </div>
+
+    <div class="app-elements-pitch__bottom">
+      <TextsP2 color="orange-100">
+        Des véhicules d’exception pour des moments uniques.
+      </TextsP2>
+
+      <AtomsCTA>
+        Découvrir la collection
+      </AtomsCTA>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.app-elements-pitch {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  position: relative;
+  padding: desktop-vw(40px) desktop-vw(24px);
+  gap: desktop-vw(40px);
+
+  &__top {
+    width: 100%;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+  }
+
+  &__bottom {
+    display: flex;
+    flex-direction: column;
+    align-self: flex-end;
+    gap: desktop-vw(24px);
+
+    .app-atom-cta {
+      align-self: flex-end;
+    }
+  }
+}
+</style>
--- web/app/pages/index.vue
+++ web/app/pages/index.vue
@@ -6,6 +6,7 @@ const { fontsLoaded } = toRefs(appStore)
 <template>
   <main class="page-main">
     <ElementsHero />
+    <ElementsPitch />
 
     <div class="app-debug-wip" style="width: 100%;height: 300vh;background-color: red;">
       <TextsH1 v-for="i in 3" :key="i">
