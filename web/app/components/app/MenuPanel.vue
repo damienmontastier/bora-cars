56917c5bb91d42022b31be0eb2bc0b6c07208f72
Damien Montastier <montastier.damien@gmail.com>
2026-04-08 00:45:17 +0200

    wip

--- web/app/components/app/MenuPanel.vue
+++ web/app/components/app/MenuPanel.vue
@@ -6,6 +6,7 @@ const { menuOpen, menuTheme } = toRefs(appStore)
 
 const backgroundRef = ref(null)
 const navRef = ref(null)
+const itemsRef = ref(null)
 
 let anim = null
 
@@ -30,7 +31,7 @@ watch(menuOpen, (open) => {
     anim = gsap.timeline({
       onComplete: () => { anim = null },
     })
-      .to(items, { y: '110%', duration: 0.3, ease: 'power3.in', stagger: 0.04 }, 0)
+      .to(items, { y: '110%', duration: 0.3, ease: 'power3.in', stagger: { each: 0.04, from: 'end' } }, 0)
       .to(backgroundRef.value, { scale: 0, duration: 0.4, ease: 'power3.inOut', transformOrigin: 'top center' }, 0.3)
   }
 })
@@ -43,35 +44,64 @@ onUnmounted(() => {
 <template>
   <div class="app-menu-panel" :class="{ 'is-open': menuOpen }">
     <div ref="backgroundRef" class="app-menu-panel__background" />
-    <nav ref="navRef" class="app-menu-panel__nav">
-      <div class="app-menu-panel__nav-mask">
-        <div class="app-menu-panel__nav-inner">
-          <UtilsBaseLink>
-            <TextsCTAXL :color="menuTheme">
-              Catalogue
-            </TextsCTAXL>
-          </UtilsBaseLink>
+    <div ref="navRef" class="app-menu-panel__nav">
+      <div ref="itemsRef" class="app-menu-panel__items">
+        <div class="app-menu-panel__nav-mask">
+          <div class="app-menu-panel__nav-inner">
+            <UtilsBaseLink>
+              <TextsCTAXL :color="menuTheme">
+                Propriétaire
+              </TextsCTAXL>
+            </UtilsBaseLink>
+          </div>
+        </div>
+        <div class="app-menu-panel__nav-mask">
+          <div class="app-menu-panel__nav-inner">
+            <UtilsBaseLink>
+              <TextsCTAXL :color="menuTheme">
+                Professionnel
+              </TextsCTAXL>
+            </UtilsBaseLink>
+          </div>
+        </div>
+        <div class="app-menu-panel__nav-mask">
+          <div class="app-menu-panel__nav-inner">
+            <UtilsBaseLink>
+              <TextsCTAXL :color="menuTheme">
+                Particulier
+              </TextsCTAXL>
+            </UtilsBaseLink>
+          </div>
+        </div>
+
+        <div class="app-menu-panel__nav-mask">
+          <div class="app-menu-panel__nav-inner">
+            <UtilsBaseLink>
+              <TextsCTAXL :color="menuTheme">
+                Contact
+              </TextsCTAXL>
+            </UtilsBaseLink>
+          </div>
         </div>
       </div>
       <div class="app-menu-panel__nav-mask">
-        <div class="app-menu-panel__nav-inner">
+        <div class="app-menu-panel__nav-inner app-menu-panel__nav__bottom">
           <UtilsBaseLink>
-            <TextsCTAXL :color="menuTheme">
-              Services
-            </TextsCTAXL>
+            <TextsCTA :color="menuTheme">
+              Génève
+            </TextsCTA>
+          </UtilsBaseLink>
+
+          <div class="app-menu-panel__nav__bottom-divider" :style="{ background: menuTheme }" />
+
+          <UtilsBaseLink>
+            <TextsCTA :color="menuTheme">
+              Paris
+            </TextsCTA>
           </UtilsBaseLink>
         </div>
       </div>
-      <div class="app-menu-panel__nav-mask">
-        <div class="app-menu-panel__nav-inner">
-          <UtilsBaseLink>
-            <TextsCTAXL :color="menuTheme">
-              Reprise
-            </TextsCTAXL>
-          </UtilsBaseLink>
-        </div>
-      </div>
-    </nav>
+    </div>
   </div>
 </template>
 
@@ -102,6 +132,12 @@ onUnmounted(() => {
     display: flex;
     flex-direction: column;
     padding: desktop-vw(28px) desktop-vw(26px);
+    gap: desktop-vw(32px);
+  }
+
+  &__items {
+    display: flex;
+    flex-direction: column;
     gap: desktop-vw(16px);
   }
 
@@ -112,6 +148,28 @@ onUnmounted(() => {
   &__nav-inner {
     display: block;
     will-change: transform;
+    transition: opacity 0.35s var(--ease-out-cubic);
+  }
+
+  &__nav__bottom {
+    display: flex;
+    align-items: center;
+    gap: desktop-vw(12px);
+  }
+
+  &__nav__bottom-divider {
+    width: desktop-vw(28px);
+    height: 2px;
+  }
+
+  &__nav:has(.app-menu-panel__nav-inner:hover) {
+    .app-menu-panel__nav-inner {
+      opacity: 0.2;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
   }
 }
 </style>
