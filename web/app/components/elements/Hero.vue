6ef45e6e527ffc86575535c736165b1433b996da
Damien Montastier <montastier.damien@gmail.com>
2026-04-09 22:57:49 +0200

    wip

--- web/app/components/elements/Hero.vue
+++ web/app/components/elements/Hero.vue
@@ -186,7 +186,7 @@ onUnmounted(() => {
 <template>
   <div class="app-elements-hero">
     <div class="app-elements-hero__background-wrapper">
-      <ElementsMedia class="app-elements-hero__background" src="/img/placeholder/hero.png" alt="test" />
+      <ElementsMedia class="app-elements-hero__background" src="/img/placeholder/hero.png" alt="test" :preload="true" />
     </div>
 
     <div class="app-elements-hero__content">
